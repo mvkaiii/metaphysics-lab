7cd5ba8234061f23ebd2eb0d32246bca3f5f1a6b
mvkaiii <f28czcstks@privaterelay.appleid.com>
2026-08-20 20:33:14 +0800

    test: cover shared ziwei palace mapping helper

--- tests/test_engine_module_layout.py
+++ tests/test_engine_module_layout.py
@@ -5,7 +5,7 @@ from zoneinfo import ZoneInfo
 from engine.project_bazi_calendar import bazi_pillars as legacy_bazi_pillars
 from engine.project_ziwei_month import project_derived_ziwei_month as legacy_ziwei_month
 from engine.bazi.calendar import bazi_pillars as modular_bazi_pillars
-from engine.ziwei.common import ZHI, PALACE_NAMES
+from engine.ziwei.common import ZHI, PALACE_NAMES, palaces_from_ming_branch
 from engine.ziwei.month import project_derived_ziwei_month as modular_ziwei_month
 
 
@@ -29,6 +29,13 @@ class EngineModuleLayoutTests(unittest.TestCase):
         self.assertEqual(ZHI, tuple("子丑寅卯辰巳午未申酉戌亥"))
         self.assertEqual(PALACE_NAMES[0], "命宮")
         self.assertEqual(PALACE_NAMES[-1], "父母宮")
+
+    def test_ziwei_common_maps_palaces_from_ming_branch(self):
+        palaces = palaces_from_ming_branch("卯")
+        self.assertEqual(palaces["命宮"], "卯")
+        self.assertEqual(palaces["兄弟宮"], "寅")
+        self.assertEqual(palaces["夫妻宮"], "丑")
+        self.assertEqual(palaces["父母宮"], "辰")
 
 
 if __name__ == "__main__":
