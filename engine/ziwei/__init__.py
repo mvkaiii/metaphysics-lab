b4f1a2c4b4adc244edc2e066390e813e23674e56
mvkaiii <f28czcstks@privaterelay.appleid.com>
2026-08-20 20:37:46 +0800

    feat: export ziwei flow-day capability

--- engine/ziwei/__init__.py
+++ engine/ziwei/__init__.py
@@ -1,4 +1,5 @@
-from .common import PALACE_NAMES, ZHI
+from .common import PALACE_NAMES, ZHI, palaces_from_ming_branch
+from .capabilities import get_capability, can_execute, should_run_by_default
 from .month import (
     annual_doujun_branch,
     effective_lunar_month,
@@ -6,13 +7,21 @@ from .month import (
     flow_month_palaces,
     project_derived_ziwei_month,
 )
+from .day import flow_day_ming_branch, flow_day_palaces, project_derived_ziwei_day
 
 __all__ = [
     "PALACE_NAMES",
     "ZHI",
+    "palaces_from_ming_branch",
+    "get_capability",
+    "can_execute",
+    "should_run_by_default",
     "annual_doujun_branch",
     "effective_lunar_month",
     "flow_month_ming_branch",
     "flow_month_palaces",
     "project_derived_ziwei_month",
+    "flow_day_ming_branch",
+    "flow_day_palaces",
+    "project_derived_ziwei_day",
 ]
