225e1df081413207c92f063d76609dcf8bf155b3
mvkaiii <f28czcstks@privaterelay.appleid.com>
2026-08-22 17:57:06 +0800

    feat: export Ziwei flowing-star public API

--- engine/ziwei/__init__.py
+++ engine/ziwei/__init__.py
@@ -10,6 +10,15 @@ from .month import (
 from .transformations import get_transformation_set
 from .flying import build_natal_flying_graph, fly_transformations
 from .natal import build_ziwei_natal
+from .flowing_stars import build_flowing_star_layer
+from .flowing_star_sources import (
+    source_from_decadal,
+    source_from_yearly,
+    source_from_monthly,
+    source_from_daily,
+    source_from_hourly,
+)
+from .flowing_star_view import materialize_flowing_star_layer, join_dynamic_cycle
 
 __all__ = [
     "PALACE_NAMES",
@@ -27,4 +36,12 @@ __all__ = [
     "build_natal_flying_graph",
     "fly_transformations",
     "build_ziwei_natal",
+    "build_flowing_star_layer",
+    "source_from_decadal",
+    "source_from_yearly",
+    "source_from_monthly",
+    "source_from_daily",
+    "source_from_hourly",
+    "materialize_flowing_star_layer",
+    "join_dynamic_cycle",
 ]
