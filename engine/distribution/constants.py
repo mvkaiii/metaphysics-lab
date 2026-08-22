49651360cf13112821976f555aadb02ab7b9adc3
mvkaiii <f28czcstks@privaterelay.appleid.com>
2026-08-23 00:12:52 +0800

    feat: define distribution runtime versions

---
+++ engine/distribution/constants.py
@@ -0,0 +1,10 @@
+"""Version constants for the portable AI distribution contract."""
+
+PROJECT_CONTRACT_VERSION = "1.0"
+RUNTIME_SCHEMA_VERSION = "1.0"
+CASE_SCHEMA_VERSION = "1.0"
+DISTRIBUTION_RUNTIME_VERSION = "1.0-exp"
+
+SUPPORTED_ACTIONS = (
+    "runtime_info",
+)
