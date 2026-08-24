6f09f7329218236891fe53a6d37187fbd374e902
mvkaiii <f28czcstks@privaterelay.appleid.com>
2026-08-24 19:55:32 +0800

    feat: add bundled vendor metadata package

---
+++ engine/vendor/__init__.py
@@ -0,0 +1,5 @@
+"""Bundled third-party vendor metadata helpers."""
+
+from .manifest import bundled_dependency, bundled_vendor_manifest
+
+__all__ = ["bundled_dependency", "bundled_vendor_manifest"]
