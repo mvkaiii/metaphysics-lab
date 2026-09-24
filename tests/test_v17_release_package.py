bbc8dae8220fabd4ed2f1ee1de160726443ba240
mvkaiii <f28czcstks@privaterelay.appleid.com>
2026-09-25 00:26:32 +0800

    test: lock v1.7.1 package identity

--- tests/test_v17_release_package.py
+++ tests/test_v17_release_package.py
@@ -8,11 +8,11 @@ from tools import build_release_package
 
 
 class V17ReleasePackageTests(unittest.TestCase):
-    def test_release_package_identity_and_members_are_v170(self):
-        self.assertEqual(build_release_package.RELEASE_VERSION, "1.7.0")
+    def test_release_package_identity_and_members_are_v171_patch(self):
+        self.assertEqual(build_release_package.RELEASE_VERSION, "1.7.1")
         self.assertEqual(
             build_release_package.USER_PACKAGE_NAME,
-            "Metaphysics-Lab-v1.7.0-User-Package.zip",
+            "Metaphysics-Lab-v1.7.1-User-Package.zip",
         )
         self.assertEqual(
             set(build_release_package.USER_ASSETS),
